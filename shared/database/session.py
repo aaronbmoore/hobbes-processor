70baefd2f4d324016dd22af3a0650779876889f2
aaronbmoore <44090590+aaronbmoore@users.noreply.github.com>
2024-10-29 16:05:23 -0400

    took webhook off this project

--- shared/database/session.py
+++ shared/database/session.py
@@ -2,8 +2,6 @@ from typing import AsyncGenerator
 from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 import ssl
-import os
-# from dotenv import load_dotenv
 from ..utils import get_database_url
 
 # Create SSL context for Neon
